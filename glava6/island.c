08aa1dcf31a370c6074aaae8b181a5f6fb68b6f6
Zakhar Mis <lalala@mail.com>
2026-04-02 14:32:53 +0700

    Надо доделать острова

--- glava6/island.c
+++ glava6/island.c
@@ -1,11 +1,32 @@
 #include <stdio.h>
-typedef struct island{
+#include <stdlib.h>
+#include <string.h>
+
+typedef struct island {
     char *name;
     char *opens;
     char *closes;
     struct island *next;
-}island;
- 
+} island;
+
+island* create(char *name){
+    island *i = malloc(sizeof(island));
+   i->name = strdup(name);
+    i->opens = "9:00";
+    i->closes = "17:00";
+    i->next = NULL;
+    return i;
+}
+
+char name[80];
+fgets(name, 80, stdin);
+island *p_island0= create(name);
+fgets(name, 80, stdin);
+island *p_island1 = create(name);
+
+island *p = malloc(sizeof(island));
+free(p);
+
 void display(island *start);
 
 int main(){
