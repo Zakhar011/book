02551fe2740ca60ab069c263427fe35c25ee31fe
Zakhar Mis <lalala@mail.com>
2026-04-09 16:38:09 +0700

    Острова доделаны

--- glava6/island.c
+++ glava6/island.c
@@ -18,33 +18,51 @@ island* create(char *name){
     return i;
 }
 
-char name[80];
-fgets(name, 80, stdin);
-island *p_island0= create(name);
-fgets(name, 80, stdin);
-island *p_island1 = create(name);
-
-island *p = malloc(sizeof(island));
-free(p);
-
 void display(island *start);
+void release(island *start);
 
 int main(){
-    island amity = {"Дружбы", "9:00", "17:00", NULL};
-    island craggy = {"Скалистый", "9:00", "17:00", NULL};
-    island isla_nublar = {"Туманный", "9:00", "17:00", NULL};
-    island shutter = {"Проклятых", "9:00", "17:00", NULL};
+    
+// char name[80];
+// fgets(name, 80, stdin);
+// island *p_island0= create(name);
+// fgets(name, 80, stdin);
+// island *p_island1 = create(name);
 
-    amity.next = &craggy;
-    craggy.next = &isla_nublar;
-    isla_nublar.next = &shutter;
+// island *p = malloc(sizeof(island));
+// free(p);
+
+// island amity = {"Дружбы", "9:00", "17:00", NULL};
+// island craggy = {"Скалистый", "9:00", "17:00", NULL};
+// island isla_nublar = {"Туманный", "9:00", "17:00", NULL};
+// island shutter = {"Проклятыхx", "9:00", "17:00", NULL};
+
+// amity.next = &craggy;
+// craggy.next = &isla_nublar;
+// isla_nublar.next = &shutter;
 
 
-    island skull = {"Остров черепа", "9:00", "17:00", NULL};
-    isla_nublar.next = &skull;
-    skull.next = &shutter;
+// island skull = {"Остров черепа", "9:00", "17:00", NULL};
+// isla_nublar.next = &skull;
+// skull.next = &shutter;
 
-    display(&amity);
+// display(&amity);
+island *start = NULL;
+island *i =NULL;
+island *next = NULL;
+char name [80];
+printf("Ctrl+D чтобы закончить):\n");
+for(; fgets(name,80,stdin) != NULL; i=next){
+    name[strcspn(name, "\n")] = 0;
+    next = create(name);
+    if (start == NULL)
+        start=next;
+    if(i!=NULL)
+    i-> next= next;
+}
+display(start);
+release(start);
+return 0;
 
 }
 void  display(island *start)
@@ -56,4 +74,14 @@ void  display(island *start)
  }
 
 
- 
+
+void release(island *start)
+{
+    island *i= start;
+    island *next =NULL;
+    for (; i!=NULL; i=next){
+        next= i-> next;
+        free(i->name);
+        free(i);
+    }
+}
